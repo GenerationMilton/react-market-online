055771340a376f26dddd2758299cd13ba292d802
Milton Muñoz <livemilton@gmail.com>
2024-11-27 09:55:50 -0500

    consume FakeStore Api to make cards in market react api

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,12 +1,33 @@
+import { useEffect, useState } from 'react'
+
 import React from 'react'
 import { Card } from '../../Components/Card'
 import { Layout } from '../../Components/Layout'
 
 export const Home = () => {
+
+    const[ items, setItems] = useState(null)
+
+    useEffect( ()=> {
+        fetch('https://api.escuelajs.co/api/v1/products')
+            .then(response => response.json())
+            .then(data => setItems(data))
+
+    }, [])
+
+
   return (
     <Layout>
         Home
-        <Card />
+        <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
+            {
+                items?.map(item => (
+                    <Card key={item.id} data={item}/>
+                ))
+            }
+        </div>
+       
+        
     </Layout>
   )
 }
