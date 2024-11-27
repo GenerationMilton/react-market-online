16d0d8495e6cc87ded224b8005cae704a51778ce
Milton Muñoz <livemilton@gmail.com>
2024-11-27 09:14:31 -0500

    add card in market app in a new component

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Card } from '../../Components/Card'
 import { Layout } from '../../Components/Layout'
 
 export const Home = () => {
   return (
     <Layout>
         Home
+        <Card />
     </Layout>
   )
 }
